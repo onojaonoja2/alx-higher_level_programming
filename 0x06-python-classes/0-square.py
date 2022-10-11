e743f19c58499c9edcc68873596741e1986c77ac
onojaonoja2 <samuelonoja06@gmail.com>
2022-10-11 12:29:47 -0700

    Initial commit

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 11 14:21:54 2022
+@author: Samuel Onoja
+"""
+
+
+class Square:
+    """Empty class Square that defines a square
+    Attributes:
+        empty
+    """
+    pass
