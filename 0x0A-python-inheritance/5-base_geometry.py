95dbabe30528cac9e67c4b418d29afca7e12ac75
onojaonoja2 <samuelonoja06@gmail.com>
2022-10-24 15:09:35 -0700

    Added file 7 txt

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 24 15:13:37 2022
+@author: Samuel Onoja
+"""
+
+
+class BaseGeometry():
+    """
+    An empty class
+    """
+    pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 24 15:13:37 2022
+@author: Samuel Onoja
+"""
+
+
+class BaseGeometry():
+    """
+    An empty class
+    """
+    pass
+
+    def area(self):
+        """
+        Public instance method that calculates the area
+        Raises:
+            Exception if area is not imlemented
+        """
+        raise Exception("area() is not implemented")
